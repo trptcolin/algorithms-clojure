f576ea8217208adaba26963d4e10493f88bb6439
Colin Jones <trptcolin@gmail.com>
2010-12-01 21:28:15 -0600

    Move specfile to work with speclj vigilant runner.

--- src/clrs/sort/insertion.clj
+++ src/clrs/sort/insertion.clj
@@ -2,7 +2,7 @@
   (:refer-clojure :exclude [sort]))
 
 (defn insert
-  ([coll n] (insert coll '() n))
+  ([coll n] (insert coll () n))
   ([head tail n]
    (if (or (empty? head)
            (> n (last head)))
@@ -10,11 +10,11 @@
      (recur (butlast head) (cons (last head) tail) n))))
 
 (defn sort
+  ([coll]
+   (sort coll []))
   ([coll-remaining coll-sorted]
    (if (empty? coll-remaining)
      coll-sorted
      (recur (rest coll-remaining)
-            (insert coll-sorted (first coll-remaining)))))
-  ([coll]
-   (sort coll '())))
+            (insert coll-sorted (first coll-remaining))))))
 
