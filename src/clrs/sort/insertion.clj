4ff74c4e0e1a4848c110a4d4b2d9e9d5dc61e807
Colin Jones <trptcolin@gmail.com>
2010-12-04 11:12:00 -0600

    Reduce > loop-recur.

--- src/clrs/sort/insertion.clj
+++ src/clrs/sort/insertion.clj
@@ -10,14 +10,11 @@
      (recur (butlast head) (cons (last head) tail) n order))))
 
 (defn sort
-  ([coll] (sort coll [] <))
-  ([coll order] (sort coll [] order))
-  ([coll-remaining coll-sorted order]
-   (if (empty? coll-remaining)
-     coll-sorted
-     (recur (rest coll-remaining)
-            (insert coll-sorted (first coll-remaining) order)
-            order))))
+  ([coll] (sort coll <))
+  ([coll order]
+   (reduce (fn [coll-sorted n] (insert coll-sorted n order))
+           []
+           coll)))
 
 (defn reverse-sort
-  ([coll] (sort coll [] >)))
+  ([coll] (sort coll >)))
